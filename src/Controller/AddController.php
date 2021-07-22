1ddd11829f8a71f41b9268028593a49a094db038
mzhang <m.zhang@it-students.fr>
2021-07-22 17:20:11 +0200

    ajout d'un bouton detail

--- src/Controller/AddController.php
+++ src/Controller/AddController.php
@@ -8,25 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\HttpFoundation\Request;
 
+use PHPCheckstyle;
+
 use App\Form\AppType;
 use App\Entity\App;
 
 class AddController extends AbstractController
 {   
     /**
-     * @Route("/test", name="test")
+     * @Route("/", name="add")
      */
-    public function index(): Response
-    {
-        return $this->render('add/test.html.twig', [
-            'controller_name' => 'Add Controller',
-        ]);
-    }
-
-    /**
-     * @Route("/add", name="add")
-     */
-    public function add(Request $request)
+    public function add(Request $request) : Response
     {
 
         $app = new App();
@@ -73,10 +65,9 @@ class AddController extends AbstractController
                //return new Response ("Lien existant");
             //}
             
-            return $this->render('add/test.html.twig', [
-                'tests' => $phpcheckstyle.$phpdumpcheck
+            return $this->render('add/rapport.html.twig', [
+                'rapports' => $phpcheckstyle.$phpdumpcheck
             ]);
-            //return new Response('<pre>'.$phpcheckstyle.$phpdumpcheck.'</pre>');
         }
         
         return $this->render('add/index.html.twig', [
@@ -85,4 +76,40 @@ class AddController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+     /**
+     * @Route("/detail", name="detail")
+     */
+    public function detail(): Response
+    {
+
+        $options['format'] = "array"; // default format
+        $formats = explode(',', $options['format']);
+        $configFile = array(
+            'indentation' => array(
+                "type" => "spaces",
+                "number" => 2
+                )
+            );
+            
+        $repository = $this->getDoctrine()->getRepository(App::class);
+
+        $query = $repository->createQueryBuilder('app')
+        ->select('app.app_GitLink')
+        ->orderBy('app.app_pk','DESC')
+        ->getQuery();
+        $url = $query->setMaxResults(1)->getOneOrNullResult();
+
+        $url_explode = explode("/",$url['app_GitLink']);
+        $project_name = substr(__DIR__,0,-14)."public".DIRECTORY_SEPARATOR.$url_explode[count($url_explode)-1];
+        $mysourcexplode = explode(',', $project_name);
+        $style = new PHPCheckstyle\PHPCheckstyle($formats, "./checkstyle_result", $configFile, null, false, true);
+        $style->processFiles($mysourcexplode, array());
+        $detail = $style->_reporter->reporters[0]->outputFile;
+
+        return $this->render('add/detail.html.twig', [
+            'details' => print_r($detail,TRUE),
+        ]);
+    }
+
 }
